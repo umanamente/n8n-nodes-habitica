6a98e714f3d963d416d6e43a3922d113cfcbcfea
VM <gmjs@yandex.ru>
2023-04-08 18:13:48 -0700

    Get many tasks - works

--- nodes/Habitica/Habitica.node.ts
+++ nodes/Habitica/Habitica.node.ts
@@ -68,10 +68,90 @@ export class Habitica implements INodeType {
 								method: 'GET',
 								url: '=tasks/user',
 							},
+							output: {
+								postReceive: [
+									// tasks are in "data" property
+									{
+										type: 'rootProperty',
+										properties: {
+											property: 'data',
+										},
+									},
+
+								],
+							},
 						},
 					},
 				],
 				default: 'getAll',
+			},
+			{
+				displayName: 'All Types of Tasks',
+				name: 'allTypes',
+				type: 'boolean',
+				default: true,
+				description: 'Whether to get all types: daily, todo, habit and rewards tasks',
+				displayOptions: {
+					show: {
+						resource: [
+							'task',
+						],
+						operation: [
+							'getAll',
+						]
+					}
+				},
+			},
+			{
+				displayName: 'Task Type',
+				name: 'taskType',
+				type: 'options',
+				placeholder: 'All tasks',
+				default: 'habits',
+				// eslint-disable-next-line n8n-nodes-base/node-param-options-type-unsorted-items
+				options: [
+					{
+						name: 'Habits',
+						value: 'habits',
+					},
+					{
+						name: 'Dailys',
+						value: 'dailys',
+					},
+					{
+						name: 'Todos',
+						value: 'todos',
+					},
+					{
+						name: 'Rewards',
+						value: 'rewards',
+					},
+					{
+						name: 'Completed Todos',
+						value: 'completedTodos',
+					},
+				],
+				displayOptions: {
+					show: {
+						resource: [
+							'task',
+						],
+						operation: [
+							'getAll',
+						]
+					},
+					hide: {
+						allTypes: [
+							true,
+						],
+					},
+				},
+				routing: {
+					send: {
+						type: 'query',
+						property: 'type',
+					}
+				},
 			},
 
 		],
